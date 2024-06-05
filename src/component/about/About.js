7ab9c1153c24b86235eaf70327ef29b1d09360b3
littlekanine <elia.kopff3891@gmail.com>
2024-06-05 11:30:36 +0200

    start SEO

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -8,7 +8,7 @@ function About() {
 					<div className="row about-wrapper">
 						<div className="col-md-6 col-sm-12">
 							<div className="about-wrapper__image load-hidden">
-								<img src={Pdp} alt="Profile Image" className="img-fluid rounded shadow-lg" />
+								<img src={Pdp} alt="Elia Kopff" className="img-fluid rounded shadow-lg" />
 							</div>
 						</div>
 						<div className="col-md-6 col-sm-12">
@@ -19,16 +19,16 @@ function About() {
 								</p>
 								<p className="about-wrapper__info-text">Curiosity brings knowledge</p>
 								<div className="social-links about-wrapper__link">
-									<a
-										href="https://www.linkedin.com/in/elia-kopff-16064b228/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr"
-										target="_blank"
-									>
+									<a href="https://www.linkedin.com/in/elia-kopff-16064b228/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr">
+										<span className="text-hidden">Lien linkedin</span>
 										<i className="fab fa-linkedin fa-inverse icon"></i>
 									</a>
 									<a href="https://github.com/littlekanine">
+										<span className="text-hidden">Lien GitHub</span>
 										<i className="fab fa-brands fa-github icon"></i>
 									</a>
 									<a href="https://x.com/K_D3828">
+										<span className="text-hidden">Lien X</span>
 										<i className="fab fa-brands fa-x-twitter icon"></i>
 									</a>
 								</div>
--- src/component/contactForm/ContactForm.js
+++ src/component/contactForm/ContactForm.js
@@ -6,9 +6,9 @@ function ContactForm() {
 		<div>
 			<section id="contact" className="form">
 				<h1 className="section-title">Contact</h1>
-				<div class="form-button hero-cta load-hidden">
-					<a rel="noreferrer" class="animated-button" href="mailto:elia.kopff3891@gmail.com">
-						<Buttons class="animated-button" message={'If you would like to contact me'} SvgRight={ArrowRight} SvgEnvellope={Enveloppe} />
+				<div className="form-button hero-cta load-hidden">
+					<a rel="noreferrer" className="animated-button" href="mailto:elia.kopff3891@gmail.com">
+						<Buttons className="animated-button" message={'If you would like to contact me'} SvgRight={ArrowRight} SvgEnvellope={Enveloppe} />
 					</a>
 				</div>
 			</section>
