6bb5f962f76ed03e754238396d0607338262d2e6
littlekanine <elia.kopff3891@gmail.com>
2024-06-10 19:19:35 +0200

    pb scroll

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import Pdp from '../../asset/img/mwa.jpg';
+import initScrollReveal from '../../scripts/scrollReveal';
 
 function About({ darkMode }) {
+	useEffect(() => {
+		initScrollReveal();
+	}, []);
+
 	useEffect(() => {
 		console.log('Dark mode is:', darkMode ? 'enabled' : 'disabled');
 	}, [darkMode]);
